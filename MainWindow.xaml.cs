f4fa1b1477d6ece239d58a6d2e743f998d77cbc9
PepijnHogervorst <pepijnh443@gmail.com>
2020-10-05 19:37:18 +0200

    #0 Initial test MQTT library: M2Mqtt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MQTT_LED_Controller
 {
@@ -20,9 +22,59 @@ namespace MQTT_LED_Controller
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MqttClient client;
+        private static readonly string[] topics = new string[] { "LED/+/Status" };
         public MainWindow()
         {
             InitializeComponent();
+
+            // Init MQTT client
+            client = new MqttClient("127.0.0.1");
+
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            client.ConnectionClosed += Client_ConnectionClosed;
+
+            
         }
+
+        private void Client_ConnectionClosed(object sender, EventArgs e)
+        {
+            TxtDebug.Dispatcher.Invoke(() =>
+            {
+                this.TxtDebug.Text = "MQTT connection closed..";
+            });
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client.Connect("Core_LED_Controller" + Guid.NewGuid().ToString());
+
+            if (client.IsConnected)
+            {
+                client.Subscribe(topics, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                client.Disconnect();
+                client.MqttMsgPublishReceived -= Client_MqttMsgPublishReceived;
+                client.ConnectionClosed -= Client_ConnectionClosed;
+            }
+        }
+
+        #region MQTT event methods
+        private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            TxtDebug.Dispatcher.Invoke(() =>
+            {
+                this.TxtDebug.Text = $"Topic: {e.Topic} \tMessage: {Encoding.UTF8.GetString(e.Message)}";
+            });
+            
+        }
+        #endregion
+
     }
 }
